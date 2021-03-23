82c13d08170f005f30e6d6302820ff02a05477fd
yxj0312 <yanxiajun@hotmail.com>
2021-03-23 23:05:11 +0100

    add logout links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
 
-                                <a :href="route('logout')"
+                                <a href={{route('logout')}}
                                         onclick="event.preventDefault();
                                                     this.closest('form').submit();">
                                     {{ __('Log out') }}
