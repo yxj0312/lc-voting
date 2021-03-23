1d1081ce12af93c6948c77cae7de5260fd565ce3
yxj0312 <yanxiajun@hotmail.com>
2021-03-23 22:37:56 +0100

    add gavatar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,12 @@
     </head>
     <body class="font-sans text-gray-900 text-sm">
         <header class="flex item-center justify-between px-8 py-4">
-        
+            <a href="#" class="">Laracasts Logo</a>
+            <div>
+                <a href="#" class="">
+                    <img src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
+                </a>
+            </div>.
         </header>
     </body>
 </html>
