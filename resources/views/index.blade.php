37a0e1d5613506e269cec7a44229fc1b203f0ea6
yxj0312 <yanxiajun@hotmail.com>
2021-03-25 23:15:49 +0100

    add filter dropdown and search bar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,25 @@
 <x-app-layout>
     <div class="filters flex space-x-6">
         <div class="w-1/3">
-    
             <select name="category" id="category" class="w-full rounded-xl border-none px-4 py-2">
                 <option value="Category One" class="">Category One</option>
                 <option value="Category One" class="">Category One</option>
                 <option value="Category One" class="">Category One</option>
                 <option value="Category One" class="">Category One</option>
             </select>
+        </div>
+
+        <div class="w-1/3">
+            <select name="other_filters" id="other_filters" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="Filter One" class="">Filter One</option>
+                <option value="Filter One" class="">Filter One</option>
+                <option value="Filter One" class="">Filter One</option>
+                <option value="Filter One" class="">Filter One</option>
+            </select>
+        </div>
+
+        <div class="w-1/3 relative">
+            <input type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white border-none px-4 py-2 pl-8">
         </div>
     </div>
 </x-app-layout>
