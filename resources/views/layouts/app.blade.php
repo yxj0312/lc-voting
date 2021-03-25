afc7b41b1c6a0a9a4b80753c39f969cb1e2061fd
yxj0312 <yanxiajun@hotmail.com>
2021-03-25 22:34:49 +0100

    add nav items

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,9 +53,24 @@
             </div>
             <div class="w-175">
                 <nav class="flex items-center justify-between text-xs">
-                    <ul class="flex uppercase font-semibold space-x-10">
+                    <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
                         <li>
-                            <a href="" class="">All ideas(87)</a>
+                            <a href="#" class="border-b-4 pb-3 border-blue">All ideas(87)</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering (6)</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In Progress (1)</a>
+                        </li>
+                    </ul>
+
+                    <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                        <li>
+                            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented (10)</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed (55)</a>
                         </li>
                     </ul>
                 </nav>
