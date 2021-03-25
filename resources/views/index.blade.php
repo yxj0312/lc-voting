b34636aeff588d0f2a75ba726f225f5b46577176
yxj0312 <yanxiajun@hotmail.com>
2021-03-25 23:12:37 +0100

    add dropdown select

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,13 @@
 <x-app-layout>
-    <div class="filters">Index of ideas</div>
+    <div class="filters flex space-x-6">
+        <div class="w-1/3">
+    
+            <select name="category" id="category" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="Category One" class="">Category One</option>
+                <option value="Category One" class="">Category One</option>
+                <option value="Category One" class="">Category One</option>
+                <option value="Category One" class="">Category One</option>
+            </select>
+        </div>
+    </div>
 </x-app-layout>
