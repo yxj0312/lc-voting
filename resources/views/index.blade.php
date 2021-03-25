471c4e9aadc5c68ea8abd3e3bce06ecdef877bb6
yxj0312 <yanxiajun@hotmail.com>
2021-03-25 23:20:01 +0100

    add search icon from heroicons, add adjust position

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,7 +19,12 @@
         </div>
 
         <div class="w-1/3 relative">
-            <input type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white border-none px-4 py-2 pl-8">
+            <input type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white border-none placeholder-gray-900 px-4 py-2 pl-8">
+            <div class="absolute top-0 flex items-center h-full ml-2">
+                <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </div>
         </div>
     </div>
 </x-app-layout>
