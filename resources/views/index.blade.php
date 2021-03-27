d1bb58cb09e1aca937faba49a29515f80da229ba
yxj0312 <yanxiajun@hotmail.com>
2021-03-27 22:21:02 +0100

    custom css and adjust the positon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -67,7 +67,7 @@
                             </div>
                             <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
                                 <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163,163,163, .5)">
-                                <ul class="absolute w-44 font-semibold bg-white shadow-lg rounded-xl py-3">
+                                <ul class="absolute w-44 text-left font-semibold bg-white shadow-lg rounded-xl py-3 ml-8">
                                     <li class=""><a href="#" class="hover:bg-gray-100 block px-5 py-3">Mark as Spam</a></li>
                                     <li class=""><a href="#" class="hover:bg-gray-100 block px-5 py-3">Delete Post</a></li>
                                 </ul>
