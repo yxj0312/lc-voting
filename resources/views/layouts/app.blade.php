6e5f762d52c8055e3da2b6273c6b54dab13d2dd9
yxj0312 <yanxiajun@hotmail.com>
2021-03-28 22:47:25 +0200

    add categorie

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,6 +59,14 @@
                         <div>
                             <input type="text" class="text-xm w-full bg-gray-100 border-none rounded-xl placeholder-gray-900 px-4 py-2" placeholder="Your Idea">
                         </div>
+                        <div>
+                            <select name="category_add" id="category_add" class="w-full bg-gray-100 rounded-xl border-none px-4 py-2">
+                                <option value="Category One" class="">Category One</option>
+                                <option value="Category One" class="">Category One</option>
+                                <option value="Category One" class="">Category One</option>
+                                <option value="Category One" class="">Category One</option>
+                            </select>
+                        </div>
 
                     </form>
 
