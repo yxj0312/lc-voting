843ebc0ebcf25a53540eb097c98692f88bc55fbc
yxj0312 <yanxiajun@hotmail.com>
2021-03-29 22:59:05 +0200

    fix hover bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,7 +92,7 @@
                                 <span class="ml-2">Attach</span>
                             </button>
                             <button type="submit"
-                                class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue font-semibold rounded-xl border border-blue hover:border-blue-hover text-white transition duration-150 ease-in px-6 py-3"
+                                class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue font-semibold rounded-xl border border-blue hover:bg-blue-hover text-white transition duration-150 ease-in px-6 py-3"
                             >
                                 <span class="ml-2">Submit</span>
                             </button>
