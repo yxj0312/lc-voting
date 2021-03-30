c7870e8862f7b02df0ec743658c4f425e17c3a54
yxj0312 <yanxiajun@hotmail.com>
2021-03-30 20:48:56 +0200

    add idea page menu

---
+++ resources/views/show.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+    <div>
+        <a href="/" class="flex items-center font-semibold hover:underline">
+            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+            </svg>
+            <span class="ml-2">
+                All ideas
+            </span>
+        </a>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 })->name('home');
+
+Route::view('/idea', 'show');
 Route::get('/dashboard', function() {
     return 123;
 })->name('dashboard');
