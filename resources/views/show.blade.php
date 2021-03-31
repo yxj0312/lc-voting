a32c772b621e16681bf4b3ad61120b165532954b
yxj0312 <yanxiajun@hotmail.com>
2021-03-31 22:32:26 +0200

    remove noneed elements

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -109,16 +109,8 @@
                             <div class="font-bold text-gray-900">John Doe</div>
                             <div>&bull;</div>
                             <div>10 hours ago</div>
-                            <div>&bull;</div>
-                            <div>Category 1</div>
-                            <div>&bull;</div>
-                            <div class="text-gray-900">3 Comments</div>
-                            <div>&bull;</div>
                         </div>
                         <div class="flex items-center space-x-2">
-                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none border rounded-full text-center w-28 h-7 py-2 px-4">
-                                Open
-                            </div>
                             <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
                                 <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163,163,163, .5)">
                                 <ul class=" hidden absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
