5db0230cb384c3e9d65c0868aeafdb79cfe2651b
yxj0312 <yanxiajun@hotmail.com>
2021-03-31 23:24:51 +0200

    vertical line

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -88,7 +88,7 @@
         </div>
     </div>
 
-    <div class="comments-container space-y-6 ml-22 my-8">
+    <div class="comments-container relative space-y-6 ml-22 my-8">
         <div class="comment-container bg-white rounded-xl flex mt-4">
             <div class="flex flex-1 px-4 py-6">
                 <div class="flex-none">
