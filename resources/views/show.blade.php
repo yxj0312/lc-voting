fde3bf18b428af070fb4e5cc05fac952c69527f5
yxj0312 <yanxiajun@hotmail.com>
2021-04-02 00:22:09 +0200

    postion the form

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -57,33 +57,45 @@
 
     <div class="buttons-container flex items-center justify-between mt-6">
         <div class="flex items-center space-x-4 ml-6">
-            <button type="button"
-                class="flex items-center justify-center h-11 w-32 text-sm bg-blue font-semibold rounded-xl border border-blue hover:bg-blue-hover text-white transition duration-150 ease-in px-6 py-3"
-            >
-                Reply
-            </button>
+            <div class="relative">
+                <button type="button"
+                    class="flex items-center justify-center h-11 w-32 text-sm bg-blue font-semibold rounded-xl border border-blue hover:bg-blue-hover text-white transition duration-150 ease-in px-6 py-3"
+                >
+                    Reply
+                </button>
 
-            <div class="absolute z-10 w-104 text-left font-semibold text-sm bg-white shadow-dialog rounded-xl mt-2">
-                <form action="#" class="space-y-4 px-4 py-6">
-                    <div>
-                        <textarea name="post_comment" id="post_comment" cols="30" rows="4"
-                            class="w-full text-sm bg-gray-100 rounded-xl placeholder-gray-900 border-none px-4 py-2"
-                            placeholder="Go ahead, don't be shy. Share your thoughts.."
-                        ></textarea>
-                    </div>
+                <div class="absolute z-10 w-104 text-left font-semibold text-sm bg-white shadow-dialog rounded-xl mt-2">
+                    <form action="#" class="space-y-4 px-4 py-6">
+                        <div>
+                            <textarea name="post_comment" id="post_comment" cols="30" rows="4"
+                                class="w-full text-sm bg-gray-100 rounded-xl placeholder-gray-900 border-none px-4 py-2"
+                                placeholder="Go ahead, don't be shy. Share your thoughts.."
+                            ></textarea>
+                        </div>
 
-                    <div class="flex items-center space-x-3">
-                        <button type="button"
-                            class="flex items-center justify-center h-11 w-32 text-sm bg-blue font-semibold rounded-xl border border-blue hover:bg-blue-hover text-white transition duration-150 ease-in px-6 py-3"
-                        >
-                            Post Comment
-                        </button>
+                        <div class="flex items-center space-x-3">
+                            <button type="button"
+                                class="flex items-center justify-center h-11 w-1/2 text-sm bg-blue font-semibold rounded-xl border border-blue hover:bg-blue-hover text-white transition duration-150 ease-in px-6 py-3"
+                            >
+                                Post Comment
+                            </button>
 
 
-                    </div>
+                            <button type="button"
+                                class="flex items-center justify-center w-32 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3"
+                            >
+                                <svg class="text-gray-600 w-4 transform -rotate-45 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                                </svg>
+                                <span class="ml-2">Attach</span>
+                            </button>
+                        </div>
 
-                </form>
+                    </form>
+                </div>
             </div>
+
+            
 
             <button type="button"
                 class="flex items-center justify-center w-36 h-11 text-sm bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3"
