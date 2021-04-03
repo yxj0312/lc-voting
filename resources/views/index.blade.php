2f0a9414e22fd7045cf0306f718b0b5830ba979d
yxj0312 <yanxiajun@hotmail.com>
2021-04-03 22:11:16 +0200

    add transition effect

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -70,12 +70,12 @@
                                 Open
                             </div>
                             <button
-                                x-cloak
                                 @click="isOpen = !isOpen"
                                 class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
                                 <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163,163,163, .5)">
-                                <ul 
-                                    x-show="isOpen"
+                                <ul
+                                    x-cloak
+                                    x-show.transition.origin.top.left.500ms="isOpen"
                                     @click.away="isOpen = false"
                                     @keydown.escape.window = "isOpen = false"
                                     class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
