b88435884451dc2558419d25b3edf3bb192bba2c
yxj0312 <yanxiajun@hotmail.com>
2021-04-03 22:15:27 +0200

    Remove the transition duration

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -75,7 +75,7 @@
                                 <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163,163,163, .5)">
                                 <ul
                                     x-cloak
-                                    x-show.transition.origin.top.left.duration.500ms="isOpen"
+                                    x-show.transition.origin.top.left.duration="isOpen"
                                     @click.away="isOpen = false"
                                     @keydown.escape.window = "isOpen = false"
                                     class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
