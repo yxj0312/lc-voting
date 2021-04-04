0ada01b4429e49d3f79f5c4d5aa3b0b11ebcf219
yxj0312 <yanxiajun@hotmail.com>
2021-04-04 22:51:14 +0200

    change filter width on mobile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div class="filters flex space-x-6">
-        <div class="w-1/3">
+    <div class="filters flex flex-col md:flex-row md:space-x-6">
+        <div class="w-full md:w-1/3">
             <select name="category" id="category" class="w-full rounded-xl border-none px-4 py-2">
                 <option value="Category One" class="">Category One</option>
                 <option value="Category One" class="">Category One</option>
@@ -9,7 +9,7 @@
             </select>
         </div>
 
-        <div class="w-1/3">
+        <div class="w-full md:w-1/3">
             <select name="other_filters" id="other_filters" class="w-full rounded-xl border-none px-4 py-2">
                 <option value="Filter One" class="">Filter One</option>
                 <option value="Filter One" class="">Filter One</option>
@@ -18,7 +18,7 @@
             </select>
         </div>
 
-        <div class="w-1/3 relative">
+        <div class="w-full md:w-2/3 relative">
             <input type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white border-none placeholder-gray-900 px-4 py-2 pl-8">
             <div class="absolute top-0 flex items-center h-full ml-2">
                 <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
