04ae5af92b5d134b2022ba32ae721427acc1ad70
yxj0312 <yanxiajun@hotmail.com>
2021-04-06 21:46:19 +0200

    give margin top between open button and infos on mobile

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -39,7 +39,7 @@
                         </div>
                         <div
                             x-data="{ isOpen: false }"
-                            class="flex items-center space-x-2">
+                            class="flex items-center space-x-2 mt-4 md:mt-0">
                             <div class="bg-gray-200 text-xxs font-bold uppercase leading-none border rounded-full text-center w-28 h-7 py-2 px-4">
                                 Open
                             </div>
