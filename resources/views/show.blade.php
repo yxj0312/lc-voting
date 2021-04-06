edf220dc3cbc6b0fdde70063367499ef2dd7b398
yxj0312 <yanxiajun@hotmail.com>
2021-04-06 21:43:55 +0200

    shorten mx to 2 on mobile

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -13,12 +13,12 @@
     <div class="ideas-container space-y-6 my-6">
         <div class="idea-container bg-white rounded-xl flex mt-4">
             <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
-                <div class="flex-none mx-4">
+                <div class="flex-none mx-2 md:mx-4">
                     <a href="#">
                         <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
                     </a>
                 </div>
-                <div class="w-full mx-4">
+                <div class="w-full mx-2 md:mx-4">
                     <h4 class="text-xl font-semibold">
                         <a href="#" class="hover:underline">A random title can go here</a>
                     </h4>
