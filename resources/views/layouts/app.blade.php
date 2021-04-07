7932862ab27eea5a78c83cced1351b6ad3c731bd
yxj0312 <yanxiajun@hotmail.com>
2021-04-07 21:53:37 +0200

    put livewire assets to app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <livewire:styles />
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -130,5 +131,6 @@
                 </div>
             </div>
         </main>
+        <livewire:scripts />
     </body>
 </html>
