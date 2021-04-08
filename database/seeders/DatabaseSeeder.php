afd07085758bed4a4d90e3fcc3ef3af55464bc83
yxj0312 <yanxiajun@hotmail.com>
2021-04-08 22:33:59 +0200

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Idea;
+use Database\Factories\IdeaFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Idea::factory(30)->create();
     }
 }
