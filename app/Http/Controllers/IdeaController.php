26548936c5bab98d0651c243411392f1e56de976
yxj0312 <yanxiajun@hotmail.com>
2021-04-08 23:03:39 +0200

    change index route, move views to idea folder

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -14,7 +14,9 @@ class IdeaController extends Controller
      */
     public function index()
     {
-        //
+        return view('idea.index', [
+            'ideas' => Idea::all()
+        ]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-})->name('home');
+Route::get('/',[IdeaController::class, 'index'])->name('idea.index');
 
 Route::view('/idea', 'show');
 Route::get('/dashboard', function() {
