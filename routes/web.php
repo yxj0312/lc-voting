fcf731326d462143b45945a1b662f33f38bd91ba
yxj0312 <yanxiajun@hotmail.com>
2021-04-09 23:48:05 +0200

    chhange route to slug

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[IdeaController::class, 'index'])->name('idea.index');
-Route::get('/ideas/{idea}',[IdeaController::class, 'show'])->name('idea.show');
+Route::get('/ideas/{idea:slug}',[IdeaController::class, 'show'])->name('idea.show');
  
 require __DIR__.'/auth.php';
