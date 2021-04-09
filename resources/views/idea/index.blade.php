5dcc78bcd6388cb540bd0f6760a0c1e9d7499395
yxj0312 <yanxiajun@hotmail.com>
2021-04-09 23:10:26 +0200

    add links to idea show

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -49,7 +49,7 @@
                     </div>
                     <div class="w-full flex flex-col justify-between mx-4 ">
                         <h4 class="text-xl font-semibold mt-2 md:mt-0">
-                            <a href="#" class="hover:underline">{{ $idea->title }}</a>
+                            <a href="{{ route('idea.show', $idea)}}" class="hover:underline">{{ $idea->title }}</a>
                         </h4>
                         <div class="text-gray-600 mt-3 line-clamp-3">
                             {{ $idea->description }} 
