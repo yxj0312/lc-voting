8257e30d73eb1f1c9f3dc85445272013f4304217
yxj0312 <yanxiajun@hotmail.com>
2021-04-10 23:27:17 +0200

    add test for slug

--- tests/Feature/ShowIdeasTest.php
+++ tests/Feature/ShowIdeasTest.php
@@ -72,4 +72,29 @@ class ShowIdeasTest extends TestCase
         $response->assertDontSee($ideaOne->title);
         $response->assertSee($ideaEleven->title);
     }
+
+
+    /** @test */
+    function same_idea_title_different_slugs()
+    {
+        $ideaOne = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea',
+        ]);
+
+        $ideaTwo = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'description' => 'Another Description for my first idea',
+        ]);
+
+        $response = $this->get(route('idea.show', $ideaOne));
+
+        $response->assertSuccessful();
+        $this->assertTrue(request()->path() === 'ideas/my-first-idea');
+
+        $response = $this->get(route('idea.show', $ideaTwo));
+        $response->assertSuccessful();
+
+        $this->assertTrue(request()->path() === 'ideas/my-first-idea-2');
+    }
 }
