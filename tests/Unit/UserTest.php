f05ea8289f695743998c9874752ae6ef01d3e6c4
yxj0312 <yanxiajun@hotmail.com>
2021-04-14 21:08:05 +0200

    avatar gen test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,16 +3,32 @@
 namespace Tests\Unit;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-   /** @test */
-   function user_can_generate_gravatar_default_image_when_no_email_found()
-   {
-       $user = User::factory()->create([
-           'name' => 'Andre',
-           'email' => 'afakemail@fakemail.com'
-       ]);
-   }
+   use RefreshDatabase;
+
+    /** @test */
+    public function user_can_generate_gravatar_defualt_image_when_no_email_found_first_character_a()
+    {
+        $user = User::factory()->create([
+            'name' => 'Andre',
+            'email' => 'afakeemail@fakeemail.com',
+        ]);
+
+        $gravatarUrl = $user->getAvatar();
+
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/'.md5($user->email).'?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-1.png',
+            $gravatarUrl
+        );
+
+        $response = Http::get($user->getAvatar());
+
+        $this->assertTrue($response->successful());
+    }
+
 }
