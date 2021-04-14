5187f9b6e759834b67c4a0ab15affd20cb2e3e4d
yxj0312 <yanxiajun@hotmail.com>
2021-04-14 21:08:38 +0200

    other test for gravtar

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -31,4 +31,63 @@ class UserTest extends TestCase
         $this->assertTrue($response->successful());
     }
 
+     /** @test */
+    public function user_can_generate_gravatar_defualt_image_when_no_email_found_first_character_z()
+    {
+        $user = User::factory()->create([
+            'name' => 'Andre',
+            'email' => 'zfakeemail@fakeemail.com',
+        ]);
+
+        $gravatarUrl = $user->getAvatar();
+
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/'.md5($user->email).'?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-26.png',
+            $gravatarUrl
+        );
+
+        $response = Http::get($user->getAvatar());
+
+        $this->assertTrue($response->successful());
+    }
+
+    /** @test */
+    public function user_can_generate_gravatar_defualt_image_when_no_email_found_first_character_0()
+    {
+        $user = User::factory()->create([
+            'name' => 'Andre',
+            'email' => '0fakeemail@fakeemail.com',
+        ]);
+
+        $gravatarUrl = $user->getAvatar();
+
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/'.md5($user->email).'?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-27.png',
+            $gravatarUrl
+        );
+
+        $response = Http::get($user->getAvatar());
+
+        $this->assertTrue($response->successful());
+    }
+
+    /** @test */
+    public function user_can_generate_gravatar_defualt_image_when_no_email_found_first_character_9()
+    {
+        $user = User::factory()->create([
+            'name' => 'Andre',
+            'email' => '9fakeemail@fakeemail.com',
+        ]);
+
+        $gravatarUrl = $user->getAvatar();
+
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/'.md5($user->email).'?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-36.png',
+            $gravatarUrl
+        );
+
+        $response = Http::get($user->getAvatar());
+
+        $this->assertTrue($response->successful());
+    }
 }
