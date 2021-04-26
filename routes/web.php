ea836301760f364c9d76ac57e31e01fa339010fe
yxj0312 <yanxiajun@hotmail.com>
2021-04-26 17:46:06 +0200

    add select-test implemetion

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[IdeaController::class, 'index'])->name('idea.index');
 Route::get('/ideas/{idea:slug}',[IdeaController::class, 'show'])->name('idea.show');
+Route::view('/select-test','select-test');
  
 require __DIR__.'/auth.php';
