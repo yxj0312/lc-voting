310f42500fe182cc7e21b5eb0cc85ee2ae4c933f
yxj0312 <yanxiajun@hotmail.com>
2021-04-28 21:07:50 +0200

    add create status migration

--- database/migrations/2021_04_27_203911_create_statuses_table.php
+++ database/migrations/2021_04_27_203911_create_statuses_table.php
@@ -15,6 +15,8 @@ class CreateStatusesTable extends Migration
     {
         Schema::create('statuses', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('classes');
             $table->timestamps();
         });
     }
