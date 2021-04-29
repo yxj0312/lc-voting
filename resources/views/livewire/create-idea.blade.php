704a41498f2c47bcf09e02623ce29e18eeb7540f
yxj0312 <yanxiajun@hotmail.com>
2021-04-29 22:24:14 +0200

    add flash message

--- resources/views/livewire/create-idea.blade.php
+++ resources/views/livewire/create-idea.blade.php
@@ -41,4 +41,21 @@
             <span class="ml-2">Submit</span>
         </button>
     </div>
+
+    <div>
+        @if (session('success_message'))
+            <div
+                x-data="{ isVisible: true }"
+                x-init="
+                    setTimeout(() => {
+                        isVisible = false
+                    }, 5000)
+                "
+                x-show.transition.duration.1000ms="isVisible"
+                class="text-green mt-4"
+            >
+                {{ session('success_message') }}
+            </div>
+        @endif
+    </div>
 </form>
