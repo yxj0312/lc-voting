4ed1a54c8d32af4f0c1b35816658c34245789f8d
yxj0312 <yanxiajun@hotmail.com>
2021-04-29 21:39:39 +0200

    add options for category

--- resources/views/livewire/create-idea.blade.php
+++ resources/views/livewire/create-idea.blade.php
@@ -5,10 +5,9 @@
     </div>
     <div>
         <select wire:model.defer="category" name="category_add" id="category_add" class="w-full bg-gray-100 text-sm rounded-xl border-none px-4 py-2">
-            <option value="Category One" class="">Category One</option>
-            <option value="Category One" class="">Category One</option>
-            <option value="Category One" class="">Category One</option>
-            <option value="Category One" class="">Category One</option>
+            @foreach ($categories as $category)
+                <option value="{{ $category->id }}">{{ $category->name }}</option>
+            @endforeach
         </select>
     </div>
 
