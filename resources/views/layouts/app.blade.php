b64ae9abe3c7d68e03224593171ac05ce1a2dd92
yxj0312 <yanxiajun@hotmail.com>
2021-04-29 21:00:38 +0200

    add a login/sign up button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,6 +70,7 @@
                         </p>
                     </div>
 
+                    @auth
                     <form action="" method="post" class="space-y-4 px-4 py-6">
                         <div>
                             <input type="text" class="text-xm w-full bg-gray-100 border-none rounded-xl placeholder-gray-900 px-4 py-2" placeholder="Your Idea">
@@ -105,7 +106,22 @@
                             </button>
                         </div>
                     </form>
-
+                    @else
+                        <div class="my-6 text-center">
+                            <a
+                                href="{{ route('login') }}"
+                                class="inline-block justify-center w-1/2 h-11 text-xs bg-blue text-white font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3"
+                            >
+                            <span class="ml-1">Login</span>
+                            </a>
+                            <a
+                                href="{{ route('register') }}"
+                                class="inline-block justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3 mt-4"
+                            >
+                            Sign Up
+                            </a>
+                        </div>
+                    @endauth
                 </div>
             </div>
             <div class="w-full px-2 md:w-175">
