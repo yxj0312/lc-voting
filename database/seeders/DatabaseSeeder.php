00bb7601e4078a29c16b2dbe8449826772911dd5
yxj0312 <yanxiajun@hotmail.com>
2021-04-30 22:35:57 +0200

    modify databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
+use App\Models\User;
+use App\Models\Vote;
 use Database\Factories\IdeaFactory;
 use Illuminate\Database\Seeder;
 
@@ -17,6 +19,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::factory()->create([
+            'name' => 'Andre',
+            'email' => 'andre_madarang@hotmail.com',
+        ]);
+
+        User::factory(19)->create();
         // \App\Models\User::factory(10)->create();
         Category::factory()->create(['name' => 'Category 1']);
         Category::factory()->create(['name' => 'Category 2']);
@@ -30,5 +38,18 @@ class DatabaseSeeder extends Seeder
         Status::factory()->create(['name' => 'Closed', 'classes' => 'bg-red text-white']);
 
         Idea::factory(30)->create();
+        Idea::factory(100)->create();
+
+        // Generate unique votes. Ensure idea_id and user_id are unique for each row
+        foreach (range(1, 20) as $user_id) {
+            foreach (range(1, 100) as $idea_id) {
+                if ($idea_id % 2 === 0) {
+                    Vote::factory()->create([
+                        'user_id' => $user_id,
+                        'idea_id' => $idea_id,
+                    ]);
+                }
+            }
+        }
     }
 }
