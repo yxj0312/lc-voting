beba123abad0d98ae0e721690a9157cadd74c6d5
yxj0312 <yanxiajun@hotmail.com>
2021-05-01 23:41:54 +0200

    create livewire IdeaShow

---
+++ app/Http/Livewire/IdeaShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class IdeaShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.idea-show');
+    }
+}
