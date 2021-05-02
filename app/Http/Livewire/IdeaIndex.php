8b03c1593ca69f13de5f8e1e3baffe0387529eac
yxj0312 <yanxiajun@hotmail.com>
2021-05-02 21:53:09 +0200

    add props to ideaIndex

--- app/Http/Livewire/IdeaIndex.php
+++ app/Http/Livewire/IdeaIndex.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class IdeaIndex extends Component
 {
+    public $idea;
+    public $votesCount;
+
     public function render()
     {
         return view('livewire.idea-index');
