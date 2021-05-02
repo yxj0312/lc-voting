f1495b812cfd5539c2dcbee45988c2e06c814320
yxj0312 <yanxiajun@hotmail.com>
2021-05-02 21:57:51 +0200

    change idea index

--- app/Http/Livewire/IdeaIndex.php
+++ app/Http/Livewire/IdeaIndex.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Idea;
 use Livewire\Component;
 
 class IdeaIndex extends Component
 {
     public $idea;
     public $votesCount;
+
+    public function mount(Idea $idea, $votesCount)
+    {
+        $this->idea = $idea;
+        $this->votesCount = $votesCount;
+    }
 
     public function render()
     {
