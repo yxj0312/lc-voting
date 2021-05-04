87c5ded6f5ab8b48551343abd4cce5d1d8306eb0
yxj0312 <yanxiajun@hotmail.com>
2021-05-04 22:08:30 +0200

    add hasVoted method

--- tests/Unit/IdeaTest.php
+++ tests/Unit/IdeaTest.php
@@ -2,17 +2,43 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Category;
+use App\Models\Idea;
+use App\Models\Status;
+use App\Models\User;
+use App\Models\Vote;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class IdeaTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_check_if_idea_is_voted_for_by_user()
     {
-        $this->assertTrue(true);
+        $user = User::factory()->create();
+        $userB = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea',
+        ]);
+
+        Vote::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertTrue($idea->isVotedByUser($user));
+        $this->assertFalse($idea->isVotedByUser($userB));
+        $this->assertFalse($idea->isVotedByUser(null));
     }
 }
