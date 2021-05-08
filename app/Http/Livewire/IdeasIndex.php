d555d955ad406dc9ca29e8af3348bbd182239138
yxj0312 <yanxiajun@hotmail.com>
2021-05-08 22:14:47 +0200

    create a livewire ideasIndex

---
+++ app/Http/Livewire/IdeasIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class IdeasIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.ideas-index');
+    }
+}
