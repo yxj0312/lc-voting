7ecbb87a840e8aac25c3793736d9f1e0cfbd6104
yxj0312 <yanxiajun@hotmail.com>
2021-05-08 22:29:42 +0200

    move controler

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Idea;
+use App\Models\Vote;
 use Livewire\Component;
 
 class IdeasIndex extends Component
 {
     public function render()
     {
-        return view('livewire.ideas-index');
+        $ideas = Idea::with('user', 'category', 'status')
+                ->addSelect([
+                    'voted_by_user' => Vote::select('id')
+                        ->where('user_id', auth()->id())
+                        ->whereColumn('idea_id', 'ideas.id')
+                ])
+                ->withCount('votes')
+                ->latest('id')
+                ->simplePaginate(Idea::PAGINATION_COUNT);
+
+            return response(view('livewire.ideas-index', compact('ideas')))
+                ->header('Cache-Control', 'no-cache, no-store, must-revalidate');
     }
 }
