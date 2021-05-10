2fcb0ab33ff8caf941c7afa7934d793c7bd859b2
yxj0312 <yanxiajun@hotmail.com>
2021-05-10 22:32:48 +0200

    change blade files

--- resources/views/livewire/status-filters.blade.php
+++ resources/views/livewire/status-filters.blade.php
@@ -1,22 +1,22 @@
  <nav class="hidden md:flex items-center justify-between text-xs">
     <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
         <li>
-            <a href="#" class="border-b-4 pb-3 border-blue">All ideas(87)</a>
+            <a wire:click.prevent="setStatus('All')" class="border-b-4 pb-3 border-blue">All ideas(87)</a>
         </li>
         <li>
-            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering (6)</a>
+            <a wire:click.prevent="setStatus('Considering')" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering (6)</a>
         </li>
         <li>
-            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In Progress (1)</a>
+            <a wire:click.prevent="setStatus('In Progress')" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In Progress (1)</a>
         </li>
     </ul>
 
     <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
         <li>
-            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented (10)</a>
+            <a wire:click.prevent="setStatus('Implemented')" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented (10)</a>
         </li>
         <li>
-            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed (55)</a>
+            <a wire:click.prevent="setStatus('Closed')" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed (55)</a>
         </li>
     </ul>
 </nav>
