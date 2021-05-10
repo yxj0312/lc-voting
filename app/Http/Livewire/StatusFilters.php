88f841977fdf87bbe2bef350c7c424ed638bf6b6
yxj0312 <yanxiajun@hotmail.com>
2021-05-10 22:14:19 +0200

    add props to statusFilters

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class StatusFilters extends Component
 {
+    public $status = 'All';
+
+    protected $queryString = [
+        'status',
+    ];
+    
     public function render()
     {
         return view('livewire.status-filters');
